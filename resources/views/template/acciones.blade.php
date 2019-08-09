405724ac188311b46bb57a65d04cf22413c53187
Javier <100javierr@gmail.com>
2019-08-08 19:37:03 -0500

    Avances en la pagina principal

---
+++ resources/views/template/acciones.blade.php
@@ -0,0 +1,32 @@
+<div class="card-deck">
+    <div class="card p-sm-1 bordeCardBody">
+        <img src="{{asset('Imags/lapto.webp')}}" class="card-img-top images" alt="Not Image">
+        <div class="card-body">
+            <h5 class="card-title">Arma tus horarios</h5>
+            <p class="card-text font-italic">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        <div>
+            <a role="button" class="btn btn-outline-info float-right" href="#">アームスケジュール</a>
+        </div>
+    </div>
+    <div class="card p-sm-2 bordeCardBody">
+        <img src="{{asset('Imags/administra.jpg')}}" class="card-img-top img-fluid images" alt="Not Image">
+        <div class="card-body">
+            <h5 class="card-title">Administra el horario</h5>
+            <p class="card-text font-italic">This card has supporting text below as a natural lead-in to additional content.</p>
+        </div>
+        <div >
+            <a role="button" class="btn btn-outline-info float-right" href="#">スケジュールを管理する</a>
+        </div>
+    </div>
+    <div class="card">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        </div>
+        <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+    </div>
+</div>
